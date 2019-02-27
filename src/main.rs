0cddaa19a3d27aaeb181ea7257152a317ce7f84c
John Ford <john@johnford.org>
2019-02-27 16:50:01 +0100

    A lot of new work

--- src/main.rs
+++ src/main.rs
@@ -7,51 +7,99 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
 extern crate stm32f3;
 use stm32f3::stm32f303;
 
+#[derive(Debug)]
+enum LEDState {
+    Off,
+    On,
+}
+
+struct Board {
+    peripherals: stm32f303::Peripherals
+}
+
+impl Board {
+    fn new() -> Self {
+        Board {
+            peripherals: stm32f303::Peripherals::take().unwrap(),
+        }
+    }
+
+    fn init(&mut self) {
+
+        let rcc = &self.peripherals.RCC;
+        let gpioe = &self.peripherals.GPIOE;
+ 
+        hprintln!("Enabling rcc.ahbenr");
+        rcc.ahbenr.modify(|_, w| w.iopeen().set_bit());
+
+        hprintln!("Configuring modes as outputs");
+        gpioe.moder.modify(|_, w| {
+            w.moder8().output();
+            w.moder9().output();
+            w.moder10().output();
+            w.moder11().output();
+            w.moder12().output();
+            w.moder13().output();
+            w.moder14().output();
+            w.moder15().output()
+        });       
+
+        hprintln!("Finished initializing");
+    }
+
+    fn change(&mut self, num: u8, state: LEDState) {
+        hprintln!("Changing LED {} to {:?}", num, state);
+        self.peripherals.GPIOE.odr.write(|w| {
+            match num {
+                0 => w.odr8().set_bit(),
+                1 => w.odr9().set_bit(),
+                2 => w.odr10().set_bit(),
+                3 => w.odr11().set_bit(),
+                4 => w.odr12().set_bit(),
+                5 => w.odr13().set_bit(),
+                6 => w.odr14().set_bit(),
+                7 => w.odr15().set_bit(),
+                _ => w.odr10().set_bit(),
+            }
+        });
+    }
+
+    fn ledOff(&mut self) {
+        self.peripherals.GPIOE.odr.write(|w| {
+            w.odr8().set_bit()
+        });
+    }
+}
+
+fn init () -> (stm32f3::stm32f303::RCC, stm32f3::stm32f303::GPIOE) {
+    let peripherals = stm32f303::Peripherals::take().unwrap();
+
+    let rcc = peripherals.RCC;
+    let gpioe = peripherals.GPIOE;
+
+    return (rcc, gpioe);
+}
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = stm32f303::Peripherals::take().unwrap();
+    let mut board = Board::new();
 
-    let rcc = &peripherals.RCC;
-    let gpioe = &peripherals.GPIOE;
+    board.init();
 
-    rcc.ahbenr.modify(|_, w| w.iopeen().set_bit());
-    hprintln!("Enabled GPIOE");
-
-    gpioe.odr.read();
-
-    hprintln!("Read from register");
-
-    // configure the pins as outputs
-    gpioe.moder.modify(|_, w| {
-        w.moder8().output();
-        w.moder9().output();
-        w.moder10().output();
-        w.moder11().output();
-        w.moder12().output();
-        w.moder13().output();
-        w.moder14().output();
-        w.moder15().output()
-    });
-
-    // Turn on all the LEDs in the compass
-    gpioe.odr.write(|w| {
-        w.odr8().set_bit();
-        w.odr9().set_bit();
-        w.odr10().set_bit();
-        w.odr11().set_bit();
-        w.odr12().set_bit();
-        w.odr13().set_bit();
-        w.odr14().set_bit();
-        w.odr15().set_bit()
-    });
+    board.change(0, LEDState::On);
+    board.change(1, LEDState::On);
+    board.change(2, LEDState::On);
+    board.change(3, LEDState::On);
+    board.change(4, LEDState::On);
+    board.change(5, LEDState::On);
+    board.change(6, LEDState::On);
+    board.change(7, LEDState::On);
 
     loop {
         // your code goes here
